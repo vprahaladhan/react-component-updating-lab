d32a7d7556c561a2a33adadc4b5b1781fd5958fe
prahal <vprahaladhan@gmail.com>
2021-04-07 19:27:15 +1000

    commit tree 9a80e625af59c7f3d64df7098449567ab2c23e41

--- src/Timer.js
+++ src/Timer.js
@@ -10,13 +10,22 @@ class Timer extends Component {
     };
   }
 
-  //Your code here
-
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
       this.props.updateInterval * 1000
     );
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+  
+  componentDidUpdate() {
+    this.timer.current.style =  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
   componentWillUnmount() {
